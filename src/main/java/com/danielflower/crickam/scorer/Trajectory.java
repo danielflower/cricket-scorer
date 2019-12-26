d4b8f76b670a2dced318e5a4a840ebb7c772f8f6
Daniel Flower <git@danielflower.com>
2019-12-26 11:47:24 +0800

    Cleaned up the Trajectory API

--- src/main/java/com/danielflower/crickam/scorer/Trajectory.java
+++ src/main/java/com/danielflower/crickam/scorer/Trajectory.java
@@ -1,20 +1,25 @@
 package com.danielflower.crickam.scorer;
 
+import java.util.Optional;
+
+/**
+ * The trajectory of the ball coming off the bat
+ */
 public final class Trajectory {
 	private final Integer speedInKms;
 	private final Double directionInDegreesRelativeToBatter;
 	private final Double elevationInDegrees;
-	private final Double distance;
+	private final Double distanceInMeters;
 
-	public Trajectory(Integer speedInKms, Double directionInDegreesRelativeToBatter, Double elevationInDegrees, Double distance) {
+	private Trajectory(Integer speedInKms, Double directionInDegreesRelativeToBatter, Double elevationInDegrees, Double distanceInMeters) {
 		this.speedInKms = speedInKms;
 		this.directionInDegreesRelativeToBatter = directionInDegreesRelativeToBatter;
 		this.elevationInDegrees = elevationInDegrees;
-		this.distance = distance;
+		this.distanceInMeters = distanceInMeters;
 	}
 
-
 	/**
+     * The direction that the ball was hit in.
 	 * <ul>
 	 * <li>
 	 * 0 == 360 == straight behind to the wicket keeper;
@@ -29,25 +34,89 @@ public final class Trajectory {
 	 * 270 = point (for right hander)
 	 * </li>
 	 * <li>
-	 * null = unknown
+	 * empty = unknown
 	 * </li>
 	 * </ul>
 	 */
-	public Double getDirectionInDegreesRelativeToBatter() {
-		return directionInDegreesRelativeToBatter;
+	public Optional<Double> directionInDegreesRelativeToBatter() {
+		return Optional.ofNullable(directionInDegreesRelativeToBatter);
 	}
 
-	public Double getElevationInDegrees() {
-		return elevationInDegrees;
+	public Optional<Double> elevationInDegrees() {
+		return Optional.ofNullable(elevationInDegrees);
 	}
 
-	@Override
+    public Optional<Integer> speedInKms() {
+        return Optional.ofNullable(speedInKms);
+    }
+
+    public Optional<Double> distanceInMeters() {
+        return Optional.ofNullable(distanceInMeters);
+    }
+
+    @Override
 	public String toString() {
 		return "Trajectory{" +
 				"speedInKms=" + speedInKms +
 				", directionInDegreesRelativeToBatter=" + directionInDegreesRelativeToBatter +
 				", elevationInDegrees=" + elevationInDegrees +
-				", distance=" + distance +
+				", distance=" + distanceInMeters +
 				'}';
 	}
+
+	public static Builder trajectory() {
+	    return new Builder();
+    }
+
+    public static final class Builder {
+        private Integer speedInKms;
+        private Double directionInDegreesRelativeToBatter;
+        private Double elevationInDegrees;
+        private Double distanceInMeters;
+
+        public Builder withSpeedInKms(Integer speedInKms) {
+            this.speedInKms = speedInKms;
+            return this;
+        }
+
+        /**
+         * The direction that the ball was hit in.
+         * <ul>
+         * <li>
+         * 0 == 360 == straight behind to the wicket keeper;
+         * </li>
+         * <li>
+         * 90 = square leg (for right hander);
+         * </li>
+         * <li>
+         * 180 = straight back to bowler;
+         * </li>
+         * <li>
+         * 270 = point (for right hander)
+         * </li>
+         * <li>
+         * null = unknown
+         * </li>
+         * </ul>
+         */
+        public Builder withDirectionInDegreesRelativeToBatter(Double directionInDegreesRelativeToBatter) {
+            this.directionInDegreesRelativeToBatter = directionInDegreesRelativeToBatter;
+            return this;
+        }
+
+        public Builder withElevationInDegrees(Double elevationInDegrees) {
+            this.elevationInDegrees = elevationInDegrees;
+            return this;
+        }
+
+        public Builder withDistanceInMeters(Double distanceInMeters) {
+            this.distanceInMeters = distanceInMeters;
+            return this;
+        }
+
+        public Trajectory build() {
+            return new Trajectory(speedInKms, directionInDegreesRelativeToBatter, elevationInDegrees, distanceInMeters);
+        }
+
+    }
 }
--- src/test/java/com/danielflower/crickam/scorer/TrajectoryTest.java
+++ src/test/java/com/danielflower/crickam/scorer/TrajectoryTest.java
@@ -2,6 +2,8 @@ package com.danielflower.crickam.scorer;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -9,16 +11,16 @@ class TrajectoryTest {
 
     @Test
     public void canCreate() {
-        Trajectory trajectory = aTrajectory().setDirectionInDegreesRelativeToBatter(-90.0).build();
-        assertThat(trajectory.getDirectionInDegreesRelativeToBatter(), is(-90.0));
+        Trajectory trajectory = aTrajectory().withDirectionInDegreesRelativeToBatter(-90.0).build();
+        assertThat(trajectory.directionInDegreesRelativeToBatter(), is(Optional.of(-90.0)));
     }
 
-    public static TrajectoryBuilder aTrajectory() {
-        return new TrajectoryBuilder()
-            .setDistance(10.0)
-            .setElevationInDegrees(12.0)
-            .setSpeedInKms(123)
-            .setDirectionInDegreesRelativeToBatter(90.0);
+    public static Trajectory.Builder aTrajectory() {
+        return new Trajectory.Builder()
+            .withDistanceInMeters(10.0)
+            .withElevationInDegrees(12.0)
+            .withSpeedInKms(123)
+            .withDirectionInDegreesRelativeToBatter(90.0);
     }
 
 }
