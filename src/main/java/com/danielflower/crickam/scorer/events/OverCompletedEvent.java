a850333977af17ff95c557fa35c14e3b5f472b85
Daniel Flower <git@danielflower.com>
2020-02-23 18:38:03 +0800

    Added the completed over to OverCompletedEvent

--- src/main/java/com/danielflower/crickam/scorer/events/OverCompletedEvent.java
+++ src/main/java/com/danielflower/crickam/scorer/events/OverCompletedEvent.java
@@ -1,19 +1,36 @@
 package com.danielflower.crickam.scorer.events;
 
 import com.danielflower.crickam.scorer.Match;
+import com.danielflower.crickam.scorer.Over;
 
 import java.time.Instant;
 
+import static java.util.Objects.requireNonNull;
+
 public final class OverCompletedEvent extends BaseMatchEvent {
 
-    private OverCompletedEvent(String id, Instant time, MatchEvent generatedBy) {
+    private final Over over;
+
+    private OverCompletedEvent(String id, Instant time, MatchEvent generatedBy, Over over) {
         super(id, time, generatedBy);
+        this.over = requireNonNull(over, "over");
+    }
+
+    /**
+     * @return The over that this event completes
+     */
+    public Over over() {
+        return over;
     }
 
     public static final class Builder extends BaseMatchEventBuilder<Builder, OverCompletedEvent> {
 
         public OverCompletedEvent build(Match match) {
-            return new OverCompletedEvent(id(), time(), generatedBy());
+            Over over = match.currentInnings()
+                .orElseThrow(() -> new IllegalStateException("Cannot complete an over when there is no innings in progress"))
+                .currentOver()
+                .orElseThrow(() -> new IllegalStateException("Cannot complete an over when there is no over in progress"));
+            return new OverCompletedEvent(id(), time(), generatedBy(), over);
         }
 
     }
